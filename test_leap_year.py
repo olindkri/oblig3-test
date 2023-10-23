f2c242e213c9444f552806a7fc96764fd4befd0f
Oliver Lind Kristiansen <79264222+olindkri@users.noreply.github.com>
2023-10-23 10:35:58 +0200

    Added files from oblig2.

---
+++ test_leap_year.py
@@ -0,0 +1,27 @@
+import pytest
+
+from leap_year import leapyear
+
+
+# Parametrize known years divisible by 4 but not divisible by 100 in "year", every value "year" should return True.
+@pytest.mark.parametrize("year", [2004, 1612, 2408, 2824, 3220, 1240])
+def test_leapyear_is_divisible_by_4_but_not_100(year):
+    assert leapyear(year) == True
+
+
+# Parametrize known years divisible by 400 in "year", every value "year" should return True.
+@pytest.mark.parametrize("year", [400, 800, 1200, 1600, 2000, 2400])
+def test_leapyear_is_divisible_by_400(year):
+    assert leapyear(year) == True
+
+
+# Parametrize known years not divisible by 4. Every value "year" should return False
+@pytest.mark.parametrize("year", [1019, 1267, 1895, 1993, 2003, 2023])
+def test_leapyear_is_not_divisible_by_4(year):
+    assert leapyear(year) == False
+
+
+# Parametrize known years divisible by 100 but not divisible by 400. Every value "year" should return False
+@pytest.mark.parametrize("year", [100, 600, 900, 1100, 1500, 1700, 1800, 2100])
+def test_leapyear_is_divisible_by_100_but_not_400(year):
+    assert leapyear(year) == False
